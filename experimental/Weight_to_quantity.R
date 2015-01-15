dbc87eef205aaa3cf36494af02856338b99f0d0e
Kyle Balkissoon <kylebalkissoon@gmail.com>
2015-01-15 16:11:36 -0500

    Update Weight_to_quantity.R
    turned off sharesearch as it can get rather large

--- experimental/Weight_to_quantity.R
+++ experimental/Weight_to_quantity.R
@@ -1,20 +1,3 @@
-#' Generates Portfolio share quantities based on weights, expected price, portfolio equity and fractional execution or (not)
-#'
-#' The \code{Weight_to_quantity} function goes through each symbol and adds the transaction
-#'
-#' 
-#'
-#'
-#' @param w is a vector of weights
-#' @param expected_price is the expected (or actual) execution prices
-#' @param equity is the portfolio equity
-#' @param allow_fractional is to allow fractional shares in the simulation or only whole shares
-#' @author Kyle Balkissoon {kylebalkissoon@gmail.com}
-#' @export
-
-
-
-
 Weight_to_quantity = function(w,expected_price,equity,allow_fractional=FALSE,search_area=0.05,max_equity_mult=1){
   optimal_allocation = (w*equity)/expected_price
   if(allow_fractional==TRUE){
@@ -22,7 +5,8 @@ Weight_to_quantity = function(w,expected_price,equity,allow_fractional=FALSE,sea
   }
   
   ##Find closest whole lot trade
-  ans = shareSearch(optimal_allocation = optimal_allocation,expected_price = expected_price,share_deviation = search_area,equity = equity,max_equity_mult = max_equity_mult)
+  ans = round(optimal_allocation,0)
+#   ans = shareSearch(optimal_allocation = optimal_allocation,expected_price = expected_price,share_deviation = search_area,equity = equity,max_equity_mult = max_equity_mult)
   
   return(ans)
   
