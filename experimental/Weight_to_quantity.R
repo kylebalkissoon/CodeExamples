06319eaa1ae3ee27dd316b7af65fa1f38814ea64
Kyle Balkissoon <kylebalkissoon@gmail.com>
2015-01-15 14:14:28 -0500

    Update Weight_to_quantity.R

--- experimental/Weight_to_quantity.R
+++ experimental/Weight_to_quantity.R
@@ -16,12 +16,12 @@
 
 
 Weight_to_quantity = function(w,expected_price,equity,allow_fractional=FALSE,search_area=0.05,max_equity_mult=1){
-  ##Optimal share lots = 
   optimal_allocation = (w*equity)/expected_price
   if(allow_fractional==TRUE){
     return(optimal_allocation)
   }
   
+  ##Find closest whole lot trade
   ans = shareSearch(optimal_allocation,expected_price,search_area,equity,max_equity_mult)
 
   
